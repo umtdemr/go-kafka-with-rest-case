4fd7ff729459ddd0b8cbf62283619d45eba3c7f7
umtdemr <umitde692@gmail.com>
2024-06-07 14:04:01 +0300

    add random wait time to the handlers

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,10 @@
 package server
 
-import "net/http"
+import (
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 type Server struct {
 	ListenAddr string
@@ -15,7 +19,16 @@ func (s *Server) Run() {
 	http.ListenAndServe(s.ListenAddr, s.router)
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	randomWaitTime := time.Duration(rand.Float64()*3*1000) * time.Millisecond
+	time.Sleep(randomWaitTime)
+}
+
 func Run() {
 	serv := NewServer(":8000")
+	serv.router.HandleFunc("GET /api/get", handler)
+	serv.router.HandleFunc("POST /api/post", handler)
+	serv.router.HandleFunc("PUT /api/put", handler)
+	serv.router.HandleFunc("DELETE /api/delete", handler)
 	serv.Run()
 }
