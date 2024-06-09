e6866dbf3a3a9abaa43e37c02fa3914756485a52
umtdemr <umitde692@gmail.com>
2024-06-09 12:43:01 +0300

    create kafka consumer & start server in a goroutine

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,40 +1,39 @@
 package main
 
 import (
-	"github.com/fsnotify/fsnotify"
-	"github.com/umtdemr/go-kafka-with-rest-case/pkg/logger"
+	"context"
+	"github.com/umtdemr/go-kafka-with-rest-case/pkg/kafka"
 	"github.com/umtdemr/go-kafka-with-rest-case/pkg/server"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	logger.GetLogger()
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatalf("An error has occurred while creating a file watcher: %s\n", err)
-	}
-	defer watcher.Close()
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				if event.Has(fsnotify.Write) {
-					log.Println("modified file", event.Name)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error", err)
-			}
-		}
-	}()
-	err = watcher.Add("log.log")
-	if err != nil {
-		log.Fatal(err)
-	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	go kafka.StartConsumer()
+
+	log.Println("Starting HTTP server...")
+
 	server.Run()
+	// Wait for termination signal
+	<-stop
+	log.Println("Shutting down gracefully...")
+
+	// Create a context with timeout to allow for graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	serverStop := make(chan struct{})
+	// Shut down the server
+	server.Shutdown(ctx, serverStop)
+
+	// Wait for the server to complete shutdown
+	<-serverStop
+	log.Println("HTTP server stopped.")
+
+	log.Println("All services stopped.")
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,11 +1,15 @@
 package server
 
 import (
+	"context"
 	"github.com/umtdemr/go-kafka-with-rest-case/pkg/logger"
+	"log"
 	"math/rand/v2"
 	"net/http"
 	"time"
 )
+
+var srv *http.Server
 
 type Server struct {
 	ListenAddr string
@@ -34,5 +38,23 @@ func Run() {
 	serv.router.HandleFunc("POST /api/post", handler)
 	serv.router.HandleFunc("PUT /api/put", handler)
 	serv.router.HandleFunc("DELETE /api/delete", handler)
-	serv.Run()
+
+	srv = &http.Server{
+		Addr:    serv.ListenAddr,
+		Handler: serv.router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+}
+
+func Shutdown(ctx context.Context, stop chan struct{}) {
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("HTTP server Shutdown: %v", err)
+	}
+	close(stop)
+	log.Println("HTTP server shutdown completed.")
 }
